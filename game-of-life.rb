0b5867ec478a12d117b0651f09cab29e15d21289
jumpingcloud <christopherlee.fanning@gmail.com>
2013-07-06 19:23:31 -0500

    Fixed .rvmrc

---
+++ game-of-life.rb
@@ -0,0 +1,5 @@
+# Basic File
+
+class World
+
+end
--- spec-game-of-life.rb
+++ spec-game-of-life.rb
@@ -1,6 +1,13 @@
 # spec file
 
 require 'rspec'
+require_reative 'game-of-life.rb'
 
 describe 'Game of life' do
+  context 'World' do
+    subject { World.new }
+    it 'should create a new world object' do
+      subject.is_a?(World).should be_true
+    end
+  end
 end
